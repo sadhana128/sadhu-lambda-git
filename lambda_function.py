7f5666ac4283b591b4e637efa32c88e720042544
sadhana128 <sadhanag649@gmail.com>
2025-12-05 22:54:57 +0530

    Second

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,37 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    """
+    Sadhu's Lambda Function
+    This function will be automatically deployed from GitHub
+    """
+    
+    # Parse input
+    if 'body' in event and event['body']:
+        try:
+            body = json.loads(event['body'])
+        except:
+            body = {}
+    else:
+        body = {}
+    
+    # Get name from input or use default
+    name = body.get('name', 'World')
+    
+    # Create response
+    response_message = f"Hello {name}! This is Sanjay's Lambda function."
+    
+    # Return response
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'headers': {
+            'Content-Type': 'application/json',
+            'X-Created-By': 'Sadhana'
+        },
+        'body': json.dumps({
+            'message': response_message,
+            'input': event,
+            'timestamp': context.aws_request_id,
+            'function': 'sadhu-lambda-func'
+        })
     }
